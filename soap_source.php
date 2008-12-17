41425253ace9f64d038a5c56de432d2be982dd0f
Eelco Wiersma <contact@pagebakers.nl>
2008-12-17 19:37:56 +0100

    Initial commit

---
+++ soap_source.php
@@ -0,0 +1,86 @@
+<?php
+class SoapSource extends DataSource {
+    
+    public $description = 'Soap Client DataSource';
+    
+    public $client = null;
+    public $connected = false;
+		
+    public function __construct($config) {
+        parent::__construct($config);
+        
+        $this->connect();
+    }
+    
+	public function connect() {
+		$this->client = new SoapClient($this->config['wsdl']);
+
+		if ($this->client) {
+            $this->connected = true;
+		}
+
+		return $this->connected;
+	}
+	
+	public function close() {
+        return true;
+	}
+	
+	public function listSources() {
+	   return $this->client->__getFunctions();
+	}
+	
+	public function query() {
+        $this->error = false;
+        
+        $args = func_get_args();
+        
+        $method = null;
+        $queryData = null;
+
+        if(count($args) == 2) {
+            $method = $args[0];
+            $queryData = $args[1];
+        } elseif(count($args) > 2 && !empty($args[1])) {
+            $method = $args[0];
+            $queryData = $args[1][0];
+        }
+        
+        if(!$method || !$queryData) {
+            return false;
+        }
+        
+        try {
+            $result = $this->client->__soapCall($method, $queryData);
+        } catch (SoapFault $fault) {
+            $this->error = $fault;
+        }
+        
+        if($this->error) {
+            $this->showError();
+            return false;   
+        } else {
+            return $result;
+        }
+	}
+	
+	public function getResponse() {
+	   return $this->client->__getLastResponse();
+	}
+	
+	public function getRequest() {
+    	return $this->client->__getLastRequest();
+	}
+	
+	public function showError($result = null) {
+        if (is_soap_fault($this->error) && Configure::read() > 0) {
+            trigger_error('SOAP Fault: (faultcode: ' . $this->error->faultcode . ', faultstring: ' . $this->error->faultstring . ')', E_USER_WARNING);
+        } else {
+            trigger_error('<span style = "color:Red;text-align:left"><b>SOAP Error:</b> ' . $this->error . '</span>', E_USER_WARNING);
+        }
+        if($result) {
+            e(sprintf("<p><b>Result:</b> %s </p>", $result));
+        }
+	}
+}
+?>
